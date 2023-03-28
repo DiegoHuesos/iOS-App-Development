dec4135f940f6827689ccb112e460fc17be4f417
DiegoHuesos <diegohd_98@hotmail.com>
2023-03-28 19:15:32 -0400

    Basic challenge app added

---
+++ I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by user237352 on 3/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
